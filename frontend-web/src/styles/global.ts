1cf27976a405d998956e31234fc06b50c8c67cad
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2020-08-23 21:11:38 -0300

    Initial commit

---
+++ frontend-web/src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #312e38;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  /* #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  } */
+
+  button {
+    cursor: pointer;
+  }
+`;
